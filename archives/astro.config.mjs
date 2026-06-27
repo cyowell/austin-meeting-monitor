9f2e983b1ccb01308114355471e0c57f21f819c2
Curt Yowell <charlesy@HUMX-charlesy-MBP.local>
2026-06-27 14:18:51 -0500

    Add sitemap generation and robots.txt

--- archives/astro.config.mjs
+++ archives/astro.config.mjs
@@ -1,8 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://austincouncil.app',
   outDir: '../docs/archives',
   base: '/archives',
+  integrations: [
+    sitemap({
+      customPages: ['https://austincouncil.app/']
+    })
+  ]
 });
